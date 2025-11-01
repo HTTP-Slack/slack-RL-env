a827ec3c889b80c2f98a6ac527d29280c706b240
AmoghRao21 <rao.amogh21@gmail.com>
2025-11-01 18:48:43 +0530

    New Workspace Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import WorkspaceSelection from './pages/WorkspaceSelection';
+import ProfileStep1 from './pages/ProfileStep1';
+import ProfileStep2 from './pages/ProfileStep2';
+import ProfileStep3 from './pages/ProfileStep3';
+import ProfileStep4 from './pages/ProfileStep4';
 
 const App = () => {
   return (
@@ -9,6 +13,10 @@ const App = () => {
         <Route path="/" element={<Navigate to="/signin" replace />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/home" element={<WorkspaceSelection />} />
+        <Route path="/profile-step1" element={<ProfileStep1 />} />
+        <Route path="/profile-step2" element={<ProfileStep2 />} />
+        <Route path="/profile-step3" element={<ProfileStep3 />} />
+        <Route path="/profile-step4" element={<ProfileStep4 />} />
       </Routes>
     </BrowserRouter>
   );
