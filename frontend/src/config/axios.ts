78c2df208bceec9a112b6e9815c16957ac44a77c
CRIMSONHydra <naverdo1@gmail.com>
2025-11-02 20:05:12 +0530

    axios interceptor config

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -12,11 +12,10 @@ const api = axios.create({
 // Request interceptor
 api.interceptors.request.use(
   (config) => {
-    // You can add auth token to headers here if needed
-    // const token = localStorage.getItem('token');
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
